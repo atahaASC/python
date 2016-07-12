7b8956df5f856d93d0f1cc6b84e7e2005faf5949
Ahnaf Taha <atahaasc@gmail.com>
2016-07-12 11:38:03 -0400

    Added custom_square lab

---
+++ custom_square.py
@@ -0,0 +1,15 @@
+from Myro import *
+
+init("sim")
+
+size = int(raw_input("What size do you want your square to be? (Integer only, please)"))
+
+forward(1, size)
+turnBy(90, "deg")
+forward(1, size)
+turnBy(90, "deg")
+forward(1, size)
+turnBy(90, "deg")
+forward(1, size)
+turnBy(90, "deg")
+forward(1, size)
